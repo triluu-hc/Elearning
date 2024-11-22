9a7cf4a2757b77d2fa5648598e1af2569015dd13
rim435 <triluu@kms-technology.com>
2024-11-22 16:59:22 +0700

    a

---
+++ elearning/elearning/test_celery.py
@@ -0,0 +1,15 @@
+from courses.models import Course
+from django.utils import timezone
+from datetime import timedelta
+
+old_date = timezone.now() - timedelta(days=100)
+course = Course.objects.create(
+    title='Old Course',
+    description='This course should be archived.',
+    subject_id=1,  # Replace with a valid subject ID
+)
+Course.objects.filter(id=course.id).update(updated_at=old_date)
+
+from courses.tasks import archive_old_courses
+archive_old_courses.delay()
+
